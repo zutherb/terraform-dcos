7578c69cea72db1173d66a1a62bef90a2f2e627e
Henry Brown <henry.brown@canary.is>
2017-04-17 12:33:15 -0400

    adds single ingress rule for marathon-lb service ports

--- dcos/slave_security_group.tf
+++ dcos/slave_security_group.tf
@@ -17,17 +17,15 @@ resource "aws_security_group_rule" "slave_egress_all" {
 
 resource "aws_security_group_rule" "slave_ingress_slave" {
   security_group_id = "${aws_security_group.slave.id}"
-
   type = "ingress"
   from_port = 0
   to_port = 65535
   protocol = "-1"
-  self = true
+  source_security_group_id = "${aws_security_group.slave.id}"
 }
 
 resource "aws_security_group_rule" "slave_ingress_master" {
   security_group_id = "${aws_security_group.slave.id}"
-
   type = "ingress"
   from_port = 0
   to_port = 65535
@@ -37,7 +35,6 @@ resource "aws_security_group_rule" "slave_ingress_master" {
 
 resource "aws_security_group_rule" "slave_ingress_public_slave" {
   security_group_id = "${aws_security_group.slave.id}"
-
   type = "ingress"
   from_port = 0
   to_port = 65535
@@ -45,20 +42,11 @@ resource "aws_security_group_rule" "slave_ingress_public_slave" {
   source_security_group_id = "${aws_security_group.public_slave.id}"
 }
 
-resource "aws_security_group_rule" "slave_ingress_service_10010" {
+resource "aws_security_group_rule" "slave_ingress_services" {
   security_group_id = "${aws_security_group.slave.id}"
   type        = "ingress"
-  from_port   = 10010
-  to_port     = 10010
+  from_port   = 10000
+  to_port     = 10100
   protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group_rule" "slave_ingress_service_10011" {
-  security_group_id = "${aws_security_group.slave.id}"
-  type = "ingress"
-  from_port = 10011
-  to_port = 10011
-  protocol = "-1"
   cidr_blocks = ["0.0.0.0/0"]
 }
