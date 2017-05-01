2334e0eab468e419931decd68f73ef0f50867446
Henry Brown <henry.brown@canary.is>
2017-05-01 15:07:23 -0400

    listener for 10005 on slave lb

--- dcos/slave_load_balancer.tf
+++ dcos/slave_load_balancer.tf
@@ -31,4 +31,11 @@ resource "aws_elb" "slaves" {
     lb_port = 10011
     lb_protocol = "tcp"
   }
+
+  listener {
+    instance_port = 10005
+    instance_protocol = "tcp"
+    lb_port = 10005
+    lb_protocol = "tcp"
+  }
 }
